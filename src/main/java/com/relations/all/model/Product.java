a9f86e302d0d50f1dfa9ea4d1b3169c49c5d3371
Ashutosh Pathak <ashu.p89@gmail.com>
2023-06-19 13:59:31 +0530

    committed

---
+++ src/main/java/com/relations/all/model/Product.java
@@ -0,0 +1,54 @@
+package com.relations.all.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.JdbcTypeCode;
+import org.hibernate.type.SqlTypes;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.UUID;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "product")
+public class Product {
+
+    @Id
+    @GeneratedValue
+    @JdbcTypeCode(SqlTypes.VARCHAR)
+    UUID id;
+
+    String name;
+
+    @ManyToMany
+    Set<Company> companies = new HashSet<>();
+
+    //Getters and setters
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Set<Company> getCompanies() {
+        return companies;
+    }
+
+    public void setCompanies(Set<Company> companies) {
+        this.companies = companies;
+    }
+}
